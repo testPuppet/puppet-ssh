7b5491429be0a94190c06939bd0376e37d98357d
root <root@prashantmaster.puppet.com>
2017-10-11 15:29:15 +0530

    added

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,9 @@
-class ssh::install {
-	package {'openssh-server':
-	ensure => present,	
-	}
+class ssh::install(
+  String $package_name = $::ssh::package_name,
+) {
+  package { 'ssh-package':
+    name   => $package_name,
+    ensure => present,
+  }
 }
+
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,12 @@
-class ssh::service {
-service { 'sshd':
+class ssh::service (
+  String $service_name = $::ssh::service_name,
+) {
+  service { 'ssh-service':
     ensure     => running,
+    name       => $service_name,
     enable     => true,
     hasstatus  => true,
     hasrestart =>true,
   }
-
 }
+
